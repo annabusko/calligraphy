2050505086bc4180161a280d94814d074345af1c
Anna Busko <118979722+abusko-idt@users.noreply.github.com>
2025-08-03 21:40:40 +0200

    feat: change eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,8 @@ import prettier from 'eslint-plugin-prettier';
 import tseslint from 'typescript-eslint';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import { globalIgnores } from 'eslint/config';
+import eslintPluginImport from 'eslint-plugin-import'; 
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config([
   globalIgnores(['dist', 'node_modules']),
@@ -14,22 +16,78 @@ export default tseslint.config([
   {
     files: ['**/*.{ts,tsx}'],
     plugins: {
+      '@typescript-eslint': tsPlugin,
+      import: eslintPluginImport,
       react,
+      prettier,
+      'jsx-a11y': jsxA11y,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      '@typescript-eslint': tsPlugin,
-      prettier,
     },
+    extends: ['plugin:import/recommended', 'plugin:import/typescript', 'plugin:jsx-a11y/recommended'],
     rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [['builtin', 'external'], 'internal', 'sibling', 'parent', 'index', 'unknown', 'object', 'type'],
+          'newlines-between': 'always-and-inside-groups',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
       'no-shadow': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-shadow': 'error',
       '@typescript-eslint/no-unused-vars': 'error',
-
-      'react-hooks/exhaustive-deps': 'error',
-
+      'no-param-reassign': 'off',
+      'prefer-const': 'error',
       'prettier/prettier': 'error',
-
+      'react-hooks/exhaustive-deps': 'error',
       'react/prop-types': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'consistent-return': 'off',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'default',
+          format: ['camelCase', 'strictCamelCase', 'PascalCase', 'StrictPascalCase', 'snake_case', 'UPPER_CASE'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+        {
+          selector: 'objectLiteralProperty',
+          format: null,
+        },
+        {
+          selector: [
+            'classProperty',
+            'objectLiteralProperty',
+            'typeProperty',
+            'classMethod',
+            'objectLiteralMethod',
+            'typeMethod',
+            'accessor',
+            'enumMember',
+          ],
+          format: null,
+          modifiers: ['requiresQuotes'],
+        },
+        {
+          selector: 'interface',
+          format: ['PascalCase'],
+          prefix: ['I'],
+        },
+        {
+          selector: 'typeAlias',
+          format: ['PascalCase'],
+          custom: {
+            regex: '^[A-Z][a-z][A-Za-z\\d]*$',
+            match: true,
+          },
+        },
+      ],
     },
     languageOptions: {
       parser: tseslint.parser,
